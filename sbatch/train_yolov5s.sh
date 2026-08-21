8f3c6506b4b5e4b438d394b69ab58492b978c97c
Edgar Tadevosyan <tadevosyanedgar22@gmail.com>
2026-08-21 22:36:31 +0400

    Fix conda paths, repo dir, and env activation in sbatch scripts

--- sbatch/train_yolov5s.sh
+++ sbatch/train_yolov5s.sh
@@ -6,12 +6,17 @@
 #SBATCH --gres=gpu:1
 #SBATCH --output=slurm_%j.out
 
-source /home/etadevosyan/miniconda3/etc/profile.d/conda.sh
-conda activate pcb-yolo
+export CONDA_PKGS_DIRS=/mnt/weka/etadevosyan/.conda/pkgs
+export CONDA_ENVS_PATH=/mnt/weka/etadevosyan/.conda/envs
+export YOLO_CONFIG_DIR=/mnt/weka/etadevosyan/.config/Ultralytics
+mkdir -p "$YOLO_CONFIG_DIR"
 
-cd /mnt/weka/etadevosyan/pcb-yolo/Ensemble-methods-of-YOLO-models-for-PCB-detection
+source /mnt/weka/shared-cache/miniforge3/etc/profile.d/conda.sh
+conda activate /mnt/weka/etadevosyan/.conda/envs/pcb-yolo
+
+cd /mnt/weka/etadevosyan/pcb-yolo/pcb-detection-models-training
 
 python train.py \
     --run-key yolov5s \
-    --weights yolov5su.pt \
+    --weights yolov5s.pt \
     --epochs 100 --imgsz 640 --batch 16 --workers 8
